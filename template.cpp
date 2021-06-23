58ecca3251d608564c6633574c7624b4158e360f
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2021-06-23 12:26:57 +0530

    SINGLE CHANGE

--- template.cpp
+++ template.cpp
@@ -12,16 +12,26 @@ void _print(string t) {cerr << t;}
 void _print(char t) {cerr << t;}
 void _print(double t) {cerr << t;}
 
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ", "; _print(p.second); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[  "; for (T i : v) {_print(i); cerr << "  ";} cerr << "]";}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ", "; _print(p.second); cerr << "}";}
 template <class T> void _print(set <T> v) {cerr << "[  "; for (T i : v) {_print(i); cerr << "  ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[  "; for (T i : v) {_print(i); cerr << "  ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[  "; for (auto i : v) {_print(i); cerr << "  ";} cerr << "]";}
 
-int32_t main(){
 
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie();
 
 
 
 }
+
+/*
+
+
+
+
+*/
+
+
