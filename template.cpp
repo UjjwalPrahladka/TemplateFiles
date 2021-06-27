a7ab59f3dbfa07fa762f9a67073fcdf9e6e00d58
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2021-06-27 21:58:52 +0530

    Added array debug

--- template.cpp
+++ template.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x << ": "; _print(x); cerr << endl;
+#define debug_array(x, n) cerr << #x << ": "; _print(x, n); cerr << endl;
 #else
 #define debug(x)
+#define debug_array(x, n)
 #endif
 
 void _print(int t) {cerr << t;}
@@ -17,6 +19,7 @@ template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fi
 template <class T> void _print(set <T> v) {cerr << "[  "; for (T i : v) {_print(i); cerr << "  ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[  "; for (T i : v) {_print(i); cerr << "  ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[  "; for (auto i : v) {_print(i); cerr << "  ";} cerr << "]";}
+template <class T> void _print(T v[], int n) {cerr << "[  "; for (int i = 0; i < n; i++) {_print(v[i]); cerr << "  ";} cerr << "]";}
 
 
 int32_t main() {
