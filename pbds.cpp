0308f97b331d3ae1053b6d3f3696073095018968
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2021-12-14 18:50:39 +0530

    pbds added

---
+++ pbds.cpp
@@ -0,0 +1,10 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+  
+tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+
+int main(){
+	pbds.order_of_key(x);
+	pbds.find_by_key(k);
+}
