e133fc73d206a0d6531d41f596c204aaad8c5a92
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2022-01-02 11:43:38 +0530

    Added mint to combinatorics

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -1,37 +1,54 @@
 #include<bits/stdc++.h>
-#define int long long 
 using namespace std;
+
 const int MAX = 1e6 + 6;
 const int MOD = 1e9 + 7;
-int fact[MAX+1];
-int invFact[MAX+1];
-int inv[MAX+1];
 
-int power(int a, int b){
-    int res = 1;
-    while(b > 0){
-        if(b & 1) res = 1LL * res * a % MOD;
-        a = 1LL * a * a % MOD;
-        b = b >> 1;
-    }
-    return res;
-}
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<MOD>;
+
+mint fact[MAX], invFact[MAX], inv[MAX];
 
 void precalculate(){
     fact[0] = 1;
-    for(int i = 1; i <= MAX; i++) fact[i] = 1LL * fact[i-1] * i % MOD;
+    for(int i = 1; i <= MAX; i++) fact[i] = fact[i-1] * i;
 
-    invFact[MAX] = power(fact[MAX], MOD-2);
+    invFact[MAX] = fact[MAX].inv();
     for(int i = MAX-1; i >= 0; i--)
-        invFact[i] = 1LL * invFact[i+1] * (i+1) % MOD; 
+        invFact[i] = invFact[i+1] * (i+1);
 
     inv[1] = 1;
     for(int i = 2; i <= MAX; i++)
-        inv[i] = MOD - (MOD / i) * inv[MOD % i] % MOD;
+        inv[i] = inv[MOD % i] * (-MOD / i);
 }
  
  
-int nCr(int n, int r){
-    if(n < r) return 0;
-    return 1LL * fact[n] * invFact[r] % MOD * invFact[n-r] % MOD;
+mint nCr(int n, int r){
+    if(n < r) return mint(0);
+    return fact[n] * invFact[r] * invFact[n-r];
 }
