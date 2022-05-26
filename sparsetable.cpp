181e1e0ddff485be91503b4d4e2175f18fddb7c3
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2022-05-26 19:15:05 +0530

    Minor Change to ST

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -7,12 +7,19 @@ struct SparseTable{
     vector<vector<int>> st;
     vector<int> logs;
 
-
     void init(int n){
         N = n;
         LOG = ceil(log2(n));
-        st.assign(n+1, vector<int>(LOG+1));
+        st.assign(n, vector<int>(LOG+1));
         logs.assign(n+1, 0);
+    }
+
+    int func(int x, int y){
+        return min(x, y);
+    }
+
+    int initial(){
+        return INT_MAX;
     }
 
     void build(vector<int>& a){
@@ -25,14 +32,14 @@ struct SparseTable{
 
         for (int j = 1; j <= LOG; j++)
             for (int i = 0; i + (1 << j) <= N; i++)
-                st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
+                st[i][j] = func(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
     }
 
     int query1(int L, int R){
-        int ans = INT_MAX;
+        int ans = initial();
         for (int j = LOG; j >= 0; j--) {
             if ((1 << j) <= R - L + 1) {
-                ans = min(ans, st[L][j]);
+                ans = func(ans, st[L][j]);
                 L += 1 << j;
             }
         }
@@ -42,7 +49,7 @@ struct SparseTable{
     //Idempotent
     int query2(int L, int R){
         int j = logs[R - L + 1];
-        int ans = min(st[L][j], st[R - (1 << j) + 1][j]);
+        int ans = func(st[L][j], st[R - (1 << j) + 1][j]);
         return ans;
     }
 };
