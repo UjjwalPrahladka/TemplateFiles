d30c9f887a356d63d10ba99bd1c917110a21d044
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2022-06-10 12:20:03 +0530

    Template version to lazy

--- segtree_lazy.cpp
+++ segtree_lazy.cpp
@@ -1,44 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct item{
-    int sum;
-};
 
-struct extra{
-    int sum;
-};
- 
+template <typename T, typename L> 
 struct segtree{
  
     int size_;
-    vector<item> values;
-    vector<extra> lazy;
-    item NEUTRAL_ELEMENT = {0};
-    extra NO_LAZY = {0};
+    vector<T> values;
+    vector<L> lazy;
+    T NEUTRAL_ELEMENT;
+    L NO_LAZY;
  
     void init(int n){
         size_ = 1;
         while(size_ < n) size_ *= 2;
-        values.assign(2*size_-1, {});
-        lazy.assign(2*size_-1, {});
-    }
- 
-    item merge(item a, item b){
-        return {a.sum + b.sum};
-    }
- 
-    item single(int v){
-        return {v};
+        values.assign(2*size_-1, NEUTRAL_ELEMENT);
+        lazy.assign(2*size_-1, NO_LAZY);
     }
 
     void propagate(int x, int lx, int rx){
         if(lx != rx) {
             int m = lx + (rx - lx) / 2;
-            values[2*x+1].sum += lazy[x].sum * (m-lx+1);
-            values[2*x+2].sum += lazy[x].sum * (rx-m);
-            lazy[2*x+1].sum += lazy[x].sum;
-            lazy[2*x+2].sum += lazy[x].sum;
+            values[2*x+1].prop(lazy[x], lx, m);
+            values[2*x+2].prop(lazy[x], m+1, rx);
+            lazy[2*x+1].prop(lazy[x]);
+            lazy[2*x+2].prop(lazy[x]);
         }
         lazy[x] = NO_LAZY;
     }
@@ -46,13 +32,13 @@ struct segtree{
     void build(vector<int>& a, int x, int lx, int rx){
         if(lx == rx){
             if(lx < a.size())
-                values[x] = single(a[lx]);
+                values[x] = T(a[lx]);
             return;
         }
         int m = lx + (rx - lx) / 2;
         build(a, 2 * x + 1, lx, m);
         build(a, 2 * x + 2, m + 1, rx);
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
+        values[x] = values[2 * x + 1] + values[2 * x + 2];
     }
     
     void build(vector<int>& a){
@@ -63,14 +49,14 @@ struct segtree{
         propagate(x, lx, rx);
         if(lx > r || rx < l) return;
         if(lx >= l && rx <= r) {
-            lazy[x].sum += val;
-            values[x].sum += val * (rx - lx + 1);
+            lazy[x].prop(val);
+            values[x].prop(val, lx, rx);
             return;
         }
         int m = lx + (rx - lx) / 2;
         update(val, l, r, 2 * x + 1, lx, m);
         update(val, l, r, 2 * x + 2, m+1, rx);
-        values[x] = merge(values[2*x+1], values[2*x+2]);
+        values[x] = values[2 * x + 1] + values[2 * x + 2];
 
     }
 
@@ -78,45 +64,65 @@ struct segtree{
         update(val, l, r, 0, 0, size_-1);
     }
  
-    item calc(int l, int r, int x, int lx, int rx){
+    T calc(int l, int r, int x, int lx, int rx){
         propagate(x, lx, rx);
         if(lx > r || rx < l) return NEUTRAL_ELEMENT;
         if(lx >= l && rx <= r) return values[x];
         int m = lx + (rx - lx) / 2;
-        item res1 = calc(l, r, 2 * x + 1, lx, m);
-        item res2 = calc(l, r, 2 * x + 2, m+1, rx);
-        return merge(res1, res2);
+        T res1 = calc(l, r, 2 * x + 1, lx, m);
+        T res2 = calc(l, r, 2 * x + 2, m+1, rx);
+        return res1 + res2;
     }
  
-    item calc(int l, int r){
+    T calc(int l, int r){
         return calc(l, r, 0, 0, size_-1);
     }
 };
- 
-int32_t main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    segtree ds;
-    ds.init(n);
-    while(m--){
-        int t;
-        cin >> t;
-        if(t == 1){
-            int l, r, v;
-            cin >> l >> r >> v;
-            r--;
-            ds.update(l, r, v);
-        }
-        else{
-            int l, r;
-            cin >> l >> r;
-            r--;
-            cout << ds.calc(l, r).sum << '\n';
 
-        }
+struct lazyNode{
+
+    int sum;
+
+    lazyNode() {
+        sum = 0;
     }
-}
+
+    void operator=(const lazyNode& other) {
+        sum = other.sum;
+    }
+
+    void prop(const lazyNode& other) {
+        sum += other.sum;
+    }
+
+    void prop(const int& val){
+        sum += val;
+    }
+
+};
+
+struct segNode{
+
+    int sum;
+
+    segNode(){
+        sum = 0;
+    }
+
+    segNode(int sum) : sum(sum){}
+
+    segNode operator+(const segNode& other) const{
+        segNode res;
+        res.sum = sum + other.sum;
+        return res;
+    }
+
+    void prop(const lazyNode& other, int l = 0, int r = 0) {
+        sum += (other.sum * (r - l + 1));
+    }
+
+    void prop(const int& val, int l = 0, int r = 0){
+        sum += (val * (r - l + 1));
+    }
+};
+ 
