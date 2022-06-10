b3def8c54dcd797b67e337904203eebc2520ba6d
Ujjwal Prahladka <ujjwalpb23@gmail.com>
2022-06-10 19:43:35 +0530

    Template version

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -1,25 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template <typename T>
 struct SparseTable{
 
     int LOG, N;
-    vector<vector<int>> st;
+    vector<vector<T>> st;
     vector<int> logs;
 
     void init(int n){
         N = n;
         LOG = ceil(log2(n));
-        st.assign(n, vector<int>(LOG+1));
+        st.assign(n, vector<T>(LOG+1));
         logs.assign(n+1, 0);
-    }
-
-    int func(int x, int y){
-        return min(x, y);
-    }
-
-    int initial(){
-        return INT_MAX;
     }
 
     void build(vector<int>& a){
@@ -28,18 +21,22 @@ struct SparseTable{
         for (int i = 2; i <= N; i++)
             logs[i] = logs[i/2] + 1;
 
-        for (int i = 0; i < N; i++) st[i][0] = a[i];
+        for (int i = 0; i < N; i++) {
+            st[i][0] = T(a[i]);
+        }
 
-        for (int j = 1; j <= LOG; j++)
-            for (int i = 0; i + (1 << j) <= N; i++)
-                st[i][j] = func(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
+        for (int j = 1; j <= LOG; j++){
+            for (int i = 0; i + (1 << j) <= N; i++){
+                st[i][j] = st[i][j-1] + st[i + (1 << (j - 1))][j - 1];
+            }
+        }
     }
 
-    int query1(int L, int R){
-        int ans = initial();
+    T query1(int L, int R){
+        T ans;
         for (int j = LOG; j >= 0; j--) {
             if ((1 << j) <= R - L + 1) {
-                ans = func(ans, st[L][j]);
+                ans = ans + st[L][j];
                 L += 1 << j;
             }
         }
@@ -47,9 +44,28 @@ struct SparseTable{
     }
 
     //Idempotent
-    int query2(int L, int R){
+    T query2(int L, int R){
         int j = logs[R - L + 1];
-        int ans = func(st[L][j], st[R - (1 << j) + 1][j]);
-        return ans;
+        return st[L][j] + st[R - (1 << j) + 1][j];
+    }
+};
+
+struct Node{
+    int mn;
+
+    Node(){
+        mn = INT_MAX;
+    }
+
+    Node(int mn) : mn(mn){}
+
+    Node operator+(const Node& other) const{
+        Node res;
+        res.mn = min(mn, other.mn);
+        return res;
+    }
+
+    void operator=(const Node& other){
+        mn = other.mn;
     }
 };
